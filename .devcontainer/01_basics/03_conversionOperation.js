e755e7b3e97a33f8322089528e56f9ffe70b0b42
vardaan-web <waliavardaan18@gmail.com>
2026-09-22 07:56:53 +0000

    conversion ofd datatypes

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -12,5 +12,16 @@ let valueNUMBER = Number(score)
 //true => 1; false => 0
 
 let isLoggedIn = 1
+
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "vardaan" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
